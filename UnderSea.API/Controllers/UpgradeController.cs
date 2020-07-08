379d1f039d74e1ad5e2a8cb4a9fa87e154af1abb
Rajmund Richárd Knapp <knapp.rajmund@encosoft.hu>
2020-07-08 12:25:30 +0200

    Upgrade end points

---
+++ UnderSea.API/Controllers/UpgradeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnderSea.API.DTO;
+
+namespace UnderSea.API.Controllers
+{
+    [ApiController]
+    [Route("[api/upgrade]")]
+    public class UpgradeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<UpgradeDTO> Get()
+        {
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Post(int id)
+        {
+            return NotFound();
+        }
+    }
+}
