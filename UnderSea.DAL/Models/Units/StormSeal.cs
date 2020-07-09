323ea32e5ccac95063db3bf20f7cc8d2f22a92b7
Levente Laurinyecz <laurinyecz.levente@encosoft.hu>
2020-07-09 09:07:05 +0200

    Create Unit base class

--- UnderSea.DAL/Models/Units/StormSeal.cs
+++ UnderSea.DAL/Models/Units/StormSeal.cs
@@ -4,14 +4,16 @@ using System.Text;
 
 namespace UnderSea.DAL.Models.Units
 {
-    public class StormSeal
+    public class StormSeal : Unit
     {
-        public int Id { get; set; }
-        public int Count { get; set; }
-        public int Price { get; set; }
-        public double AttackScore { get; set; }
-        public double DefenseScore { get; set; }
-        public int PearlCostPerTurn { get; set; }
-        public int CoralCostPerTurn { get; set; }
+        public StormSeal() 
+        {
+            Count = 0;
+            Price = 50;
+            AttackScore = 6.0;
+            DefenseScore = 2.0;
+            PearlCostPerTurn = 1;
+            CoralCostPerTurn = 1;
+        }
     }
 }
