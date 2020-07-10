8e6e4610d97228c8a662bcffd6a70e7f49e47126
bordakm <mate@webtar.hu>
2020-07-10 11:36:23 +0200

    army fix

--- UnderSea.BLL/Services/ArmyService.cs
+++ UnderSea.BLL/Services/ArmyService.cs
@@ -97,7 +97,6 @@ namespace UnderSea.BLL.Services
         {
             var attacks = await db.Attacks
                                .Include(attacks => attacks.UnitGroup)
-                               .ThenInclude(unitGroup => unitGroup.Units)
                                .Where(attacks => attacks.AttackerUser.Id == userId)
                                .ToListAsync();
 
@@ -120,11 +119,10 @@ namespace UnderSea.BLL.Services
         {
             var user = await db.Users
                                .Include(user => user.Country)
-                               .ThenInclude(country => country.Army)
-                               .ThenInclude(army => army.Units)
+                               .ThenInclude(country => country.DefendingArmy)
                                .FirstOrDefaultAsync(user => user.Id == userId);
 
-            var units = user.Country.Army.Units.ToList();
+            var units = user.Country.DefendingArmy;
 
             return mapper.Map<List<UnitViewModel>>(units);
         }
