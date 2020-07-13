d87dcf763259e4ba8ee6f3c41b4211960da48b56
Levente Laurinyecz <laurinyecz.levente@encosoft.hu>
2020-07-13 08:54:39 +0200

    Add GameService to ScoreBoardController

--- UnderSea.API/Controllers/ScoreboardController.cs
+++ UnderSea.API/Controllers/ScoreboardController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UnderSea.BLL.DTO;
+using UnderSea.BLL.Services;
 using UnderSea.BLL.ViewModels;
 
 namespace UnderSea.API.Controllers
@@ -14,10 +15,17 @@ namespace UnderSea.API.Controllers
     public class ScoreboardController : ControllerBase
     {
 
-        [HttpPost]
-        public ActionResult<List<ScoreboardViewModel>> Search([FromBody] SearchDTO search)
+        private readonly IGameService gameService;
+
+        public ScoreboardController(IGameService gameService)
         {
-            return NotFound(new List<ScoreboardViewModel>());
+            this.gameService = gameService;
+        }
+
+        [HttpPost]
+        public Task<List<ScoreboardViewModel>> Search([FromBody] SearchDTO search)
+        {
+            return gameService.SearchScoreboard(search);
         }
     }
 }
