f584ffb4292a524bdaef6e94f95088eb3487e5a2
bordakm <mate@webtar.hu>
2020-07-16 08:22:51 +0200

    mapper fix

--- UnderSea.API/Startup.cs
+++ UnderSea.API/Startup.cs
@@ -30,7 +30,7 @@ namespace UnderSea.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper(typeof(Startup));            
+            services.AddAutoMapper(typeof(MapperProfile));            
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
