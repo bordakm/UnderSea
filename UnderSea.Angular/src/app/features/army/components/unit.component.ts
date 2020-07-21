50f5a2d3d34cb91cb09238eec7f9eaa1821d28da
Porgányi Anna <porganyi.anna@gmail.com>
2020-07-21 16:16:37 +0200

    army

--- UnderSea.Angular/src/app/features/army/components/unit.component.ts
+++ UnderSea.Angular/src/app/features/army/components/unit.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { IUnitViewModel } from '../models/army.model';
 import { UnitViewModel } from 'src/app/shared';
 
 @Component({
