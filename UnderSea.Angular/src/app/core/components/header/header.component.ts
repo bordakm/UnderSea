4c75d421484648c86479911c40cf17c6e1e26021
Porgányi Anna <porganyi.anna@gmail.com>
2020-07-21 16:16:07 +0200

    layout data

--- UnderSea.Angular/src/app/core/components/header/header.component.ts
+++ UnderSea.Angular/src/app/core/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { LayoutService } from '../../services/layout.service';
 import { tap, catchError } from 'rxjs/operators';
 import { UnitViewModel, MainPageViewModel } from '../../../shared/index';
 import { LayoutComponent } from '../layout/layout.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-header',
@@ -13,15 +14,30 @@ import { LayoutComponent } from '../layout/layout.component';
 export class HeaderComponent implements OnInit {
 
   everything: MainPageViewModel;
-  
-  constructor(private service: LayoutService, public layout: LayoutComponent) { }
+
+  constructor(private service: LayoutService, public layout: LayoutComponent, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.service.getEverything().pipe(
       tap(res => this.everything = res),
       catchError(error => console.assert)
     ).subscribe();
-
   }
 
+  newRound(): void{
+    this.service.newRound(
+    ).subscribe(
+      res => {
+        this.snackbar.open('Új kör', 'Bezár', {
+          duration: 3000,
+          panelClass: ['my-snackbar'],
+        });
+      },
+      err => {
+        this.snackbar.open('A művelet sikertelen', 'Bezár', {
+          duration: 3000,
+          panelClass: ['my-snackbar'],
+        });
+      });
+  }
 }
--- UnderSea.Angular/src/app/core/components/profile/profile.component.ts
+++ UnderSea.Angular/src/app/core/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public layout: LayoutComponent) { }
 
   ngOnInit(): void {
   }
