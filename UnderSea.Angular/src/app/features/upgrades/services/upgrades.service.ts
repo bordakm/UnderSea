5bdd310b8f6123e3e3de07059729cf227d4a1a78
Porgányi Anna <porganyi.anna@gmail.com>
2020-07-22 12:40:52 +0200

    upgrade service

--- UnderSea.Angular/src/app/features/upgrades/services/upgrades.service.ts
+++ UnderSea.Angular/src/app/features/upgrades/services/upgrades.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { UnitViewModel, ApiClient, UpgradeViewModel } from '../../../shared/index';
+import { UnitViewModel, ApiClient, UpgradeViewModel, UpgradesClient, IdDTO } from '../../../shared/index';
 
 
 @Injectable({
@@ -11,11 +11,16 @@ import { UnitViewModel, ApiClient, UpgradeViewModel } from '../../../shared/inde
 })
 export class UpgradesService {
 
-  constructor(private http: HttpClient, private client: ApiClient) { }
+  constructor(private http: HttpClient, private client: ApiClient, private clientu: UpgradesClient) { }
 
   getUpgrades(): Observable<UpgradeViewModel[]>{
     return this.client.upgrades();
   }
 
-
+  research(id: number): Observable<any>{
+      const iddto: IdDTO = new IdDTO(
+        { id }
+      );
+      return this.clientu.research(iddto);
+  }
 }
