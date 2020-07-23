50263444be77e3681428f31f3b70b1571c3fda37
bordakm <mate@webtar.hu>
2020-07-23 16:01:11 +0200

    hangfire available from external hosts

---
+++ UnderSea.API/HangfireAuthorizationFilter.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnderSea.API
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            //return context.GetHttpContext().User.Identity.IsAuthenticated;
+            return true; // TODO ezt kiszedni, nem biztonságos..
+        }
+    }
+}
