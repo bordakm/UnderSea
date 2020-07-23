00a891984c10bf884fec89828c44c47605aa0a38
levoskaa <laurinyecz.levi@gmail.com>
2020-07-23 16:51:37 +0200

    Move navigation into subscribe

--- UnderSea.Angular/src/app/core/pages/login/login.component.ts
+++ UnderSea.Angular/src/app/core/pages/login/login.component.ts
@@ -46,14 +46,14 @@ export class LoginComponent implements OnInit {
   }
 
   login(): void {
-
-    this.authService.login(this.username, this.password).pipe(
-      tap(tokens => {
+    this.authService.login(
+      this.loginForm.controls.username.value,
+      this.loginForm.controls.password.value)
+      .subscribe(tokens => {
         if (tokens.accessToken != null) {
           this.router.navigate(['/main']);
         }
-      })
-    ).subscribe();
+      });
   }
 
   signup(): void {
@@ -61,17 +61,18 @@ export class LoginComponent implements OnInit {
       this.regForm.controls.regUsername.value,
       this.regForm.controls.regPassword.value,
       this.regForm.controls.country.value).pipe(
-      tap(res => {
+        tap(res => {
+          this.snackbar.open('Sikeres regisztráció', 'Bezár', {
+            duration: 3000,
+            panelClass: ['my-snackbar'],
+          });
+        })
+      ).subscribe(res => {
         if (res.accessToken != null) {
           this.reg = false;
           this.router.navigate(['/main']);
         }
-        this.snackbar.open('Sikeres regisztráció', 'Bezár', {
-          duration: 3000,
-          panelClass: ['my-snackbar'],
-        });
-      })
-    ).subscribe();
+      });
   }
 
   _true(): void {
@@ -83,6 +84,7 @@ export class LoginComponent implements OnInit {
     this.reg = false;
     this.regForm.reset();
   }
+  
   public noWhitespaceValidator(control: FormControl) {
     const isWhitespace = (control.value || '').trim().length === 0;
     const isValid = !isWhitespace;
