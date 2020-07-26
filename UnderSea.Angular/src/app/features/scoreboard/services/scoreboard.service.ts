04ade44d3dcd135b54cf0c308a4e8357743ef554
levoskaa <laurinyecz.levi@gmail.com>
2020-07-26 17:15:38 +0200

    Fix #20

--- UnderSea.Angular/src/app/features/scoreboard/services/scoreboard.service.ts
+++ UnderSea.Angular/src/app/features/scoreboard/services/scoreboard.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
-import { IUserViewModel } from '../models/scoreboard.model';
-
-import { UnitViewModel, ApiClient, IScoreboardViewModel, ScoreboardViewModel } from '../../../shared/index';
+import { ApiClient, ScoreboardViewModel } from '../../../shared/index';
 
 @Injectable({
     providedIn: 'root',
